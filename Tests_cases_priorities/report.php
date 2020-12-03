c4e2a4981fda8bc6a302cbf8b33a863f018c393a
ovalerio280788 <osvamo280788@gmail.com>
2020-12-03 14:20:09 -0600

    Working on the first custom report

--- Tests_cases_priorities/report.php
+++ Tests_cases_priorities/report.php
@@ -1,361 +1,339 @@
+<?php if (!defined('ROOTPATH')) exit('No direct script access allowed'); ?>
 <?php
 
 /**
- * Copyright Gurock Software GmbH. See license.md for details.
+ * Cases Property Groups report for TestRail
  *
- * This is a sample custom report plugin that computes and displays
- * the result distribution for different test properties.
+ * Copyright Gurock Software GmbH. All rights reserved.
  *
- * http://docs.gurock.com/testrail-custom/reports-introduction
+ * This is the TestRail report for displaying the test cases within
+ * a configurable scope, grouped by a configurable attribute.
+ *
  * http://www.gurock.com/testrail/
  */
 
-class Tests_cases_priorities_report_plugin extends Report_plugin
+class Cases_property_groups_report_plugin extends Report_plugin
 {
-	private $_model;
-	private $_controls;
+    private $_controls;
 
-	// The controls and options for those controls that are used on
-	// the form of this report.
-	private static $_control_schema = array(
-		'runs_select' => array(
-			'namespace' => 'custom_runs',
-			'multiple_suites' => true
-		),
-		'runs_limit' => array(
-			'type' => 'limits_select',
-			'namespace' => 'custom_runs',
-			'min' => 0,
-			'max' => 100,
-			'default' => 10
-		)
-	);
-		
-	// The resources (files) to copy to the output directory when
-	// generating a report.
-	private static $_resources = array(
-		'js/highcharts.js',
-		'js/jquery.js',
-		'styles/print.css',
-		'styles/reset.css',
-		'styles/view.css'
-	);
+    // The controls and options for those controls that are used on
+    // the form of this report.
+    private static $_control_schema = array(
+        'cases_grouping' => array(
+            'type' => 'grouping_select',
+            'namespace' => 'custom_cases',
+            'default' => 'cases:priority_id'
+        ),
+        'suites_select' => array(
+            'namespace' => 'custom_suites'
+        ),
+        'sections_select' => array(
+            'namespace' => 'custom_sections'
+        ),
+        'cases_columns' => array(
+            'type' => 'columns_select',
+            'namespace' => 'custom_cases',
+            'default' => array(
+                'cases:id' => 75,
+                'cases:title' => 0
+            )
+        ),
+        'cases_filter' => array(
+            'namespace' => 'custom_cases'
+        ),
+        'cases_limit' => array(
+            'type' => 'limits_select',
+            'namespace' => 'custom_cases',
+            'min' => 0,
+            'max' => 1000,
+            'default' => 25
+        ),
+        'content_hide_links' => array(
+            'namespace' => 'custom_content',
+        )
+    );
 
-	public function __construct()
-	{
-		parent::__construct();
-		$this->_model = new Tests_cases_priorities_summary_model();
-		$this->_model->init();
-		$this->_controls = $this->create_controls(
-			self::$_control_schema
-		);
-	}
+    // The resources to copy to the output directory when generating a
+    // report.
+    private static $_resources = array(
+        'images/report-assets/suite16.svg',
+        'images/report-assets/help.svg',
+        'js/highcharts.js',
+        'js/jquery.js',
+        'styles/print.css',
+        'styles/reset.css',
+        'styles/view.css'
+    );
 
-	public function prepare_form($context, $validation)
-	{
-		// Assign the validation rules for the controls used on the
-		// form.
-		$this->prepare_controls($this->_controls, $context, 
-			$validation);
+    public function __construct()
+    {
+        parent::__construct();
+        $this->_controls = $this->create_controls(
+            self::$_control_schema
+        );
+    }
 
-		// Assign the validation rules for the fields on the form.
-		$validation->add_rules(
-			array(
-				'custom_types_include' => array(
-					'type' => 'bool',
-					'default' => false
-				),
-				'custom_priorities_include' => array(
-					'type' => 'bool',
-					'default' => false
-				)
-			)
-		);
+    public function prepare_form($context, $validation)
+    {
+        // Assign the validation rules for the controls used on the
+        // form.
+        $this->prepare_controls($this->_controls, $context,
+            $validation);
 
-		if (request::is_post())
-		{
-			return;
-		}
+        // Assign the validation rules for the fields on the form
+        // that are not covered by the controls and are specific to
+        // this report.
+        $validation->add_rules(
+            array(
+                'custom_cases_include_summary' => array(
+                    'type' => 'bool',
+                    'default' => false
+                ),
+                'custom_cases_include_details' => array(
+                    'type' => 'bool',
+                    'default' => false
+                )
+            )
+        );
 
-		// We assign the default values for the form depending on the
-		// event. For 'add', we use the default values of this plugin.
-		// For 'edit/rerun', we use the previously saved values of
-		// the report/report job to initialize the form. Please note
-		// that we prefix all fields in the form with 'custom_' and
-		// that the storage format omits this prefix (validate_form).
+        if (request::is_post())
+        {
+            return;
+        }
 
-		if ($context['event'] == 'add')
-		{
-			$defaults = array(
-				'types_include' => true,
-				'priorities_include' => true
-			);
-		}
-		else
-		{
-			$defaults = $context['custom_options'];
-		}
+        // We assign the default values for the form depending on the
+        // event. For 'add', we use the default values of this plugin.
+        // For 'edit/rerun', we use the previously saved values of
+        // the report/report job to initialize the form. Please note
+        // that we prefix all fields in the form with 'custom_' and
+        // that the storage format omits this prefix (validate_form).
 
-		foreach ($defaults as $field => $value)
-		{
-			$validation->set_default('custom_' . $field, $value);
-		}
-	}
+        if ($context['event'] == 'add')
+        {
+            $defaults = array(
+                'cases_include_summary' => true,
+                'cases_include_details' => true
+            );
+        }
+        else
+        {
+            $defaults = $context['custom_options'];
+        }
 
-	public function validate_form($context, $input, $validation)
-	{
-		// At least one detail entity option must be selected (types or
-		// priorities).
-		if (!$input['custom_types_include'] && !$input['custom_priorities_include'])
-		{
-			$validation->add_error(
-				lang('reports_tmpl_form_details_include_required')
-			);
+        foreach ($defaults as $field => $value)
+        {
+            $validation->set_default('custom_' . $field, $value);
+        }
+    }
 
-			return false;
-		}
-				
-		// We begin with validating the controls used on the form.
-		$values = $this->validate_controls(
-			$this->_controls,
-			$context,
-			$input,
-			$validation);
+    public function validate_form($context, $input, $validation)
+    {
+        // At least one case option must be selected (summary or
+        // details).
+        if (!$input['custom_cases_include_summary'] &&
+            !$input['custom_cases_include_details'])
+        {
+            $validation->add_error(
+                lang('reports_cpg_form_cases_required')
+            );
 
-		if (!$values)
-		{
-			return false;
-		}
-		
-		static $fields = array(
-			'types_include',
-			'priorities_include'
-		);
+            return false;
+        }
 
-		// And then add our fields from the form input that are not
-		// covered by the controls and return the data as it should be
-		// stored in the report options.
-		foreach ($fields as $field)
-		{
-			$key = 'custom_' . $field;
-			$values[$field] = arr::get($input, $key);
-		}
+        // We begin with validating the controls used on the form.
+        $values = $this->validate_controls(
+            $this->_controls,
+            $context,
+            $input,
+            $validation);
 
-		return $values;
-	}
+        if (!$values)
+        {
+            return false;
+        }
 
-	public function render_form($context)
-	{
-		$params = array(
-			'controls' => $this->_controls,
-			'project' => $context['project']
-		);
+        static $fields = array(
+            'cases_include_summary',
+            'cases_include_details'
+        );
 
-		// Note that we return separate HTML snippets for the form/
-		// options and the used dialogs (which must be included after
-		// the actual form as they include their own <form> tags).
-		return array(
-			'form' => $this->render_view(
-				'form',
-				$params,
-				true
-			),
-			'after_form' => $this->render_view(
-				'form_dialogs',
-				$params,
-				true
-			)
-		);
-	}
+        // And then add our fields from the form input that are not
+        // covered by the controls and return the data as it should be
+        // stored in the report options.
+        foreach ($fields as $field)
+        {
+            $key = 'custom_' . $field;
+            $values[$field] = arr::get($input, $key);
+        }
 
-	public function run($context, $options)
-	{
-		$project = $context['project'];
+        return $values;
+    }
 
-		// Read the test suites first.
-		$suites = $this->_helper->get_suites_by_include(
-			$project->id,
-			$options['runs_suites_ids'],
-			$options['runs_suites_include']
-		);
+    private function _get_groupby($project_id, $case_columns,
+        $case_fields)
+    {
+        // We first add the built-in fields that are groupable.
+        static $columns = array(
+            'cases:created_by',
+            'cases:milestone_id',
+            'cases:priority_id',
+            'cases:template_id',
+            'cases:type_id'
+        );
 
-		$suite_ids = obj::get_ids($suites);
+        if (!fields::has_for_cases($project_id, 'milestone_id'))
+        {
+            unset($columns['cases:milestone_id']);
+        }
 
-		// We then get the actual list of test runs used, depending on
-		// the report options.
-		if ($suite_ids)
-		{
-			$runs = $this->_helper->get_runs_by_include(
-				$project->id,
-				$suite_ids,
-				$options['runs_include'],
-				$options['runs_ids'],
-				$options['runs_filters'],
-				null, // Active and completed
-				$options['runs_limit'],
-				$run_rels,
-				$run_count
-			);
-		}
-		else
-		{
-			$runs = array();
-			$run_rels = array();
-			$run_count = 0;
-		}
+        $attributes = array();
+        foreach ($columns as $key)
+        {
+            $label = arr::get($case_columns, $key);
+            if ($label)
+            {
+                $attributes[$key] = $label;
+            }
+        }
 
-		$run_ids = obj::get_ids($runs);
+        // And then add the custom fields that are groupable.
+        foreach ($case_fields as $key => $field)
+        {
+            if ($field->is_groupable())
+            {
+                $key = 'cases:' .  $key;
+                $label = arr::get($case_columns, $key);
+                if ($label)
+                {
+                    $attributes[$key] = $label;
+                }
+            }
+        }
 
-		// Get all active statuses from the database.
-		$statuses = $this->_model->get_statuses();
-		$status_ids = obj::get_ids($statuses);
+        asort($attributes);
+        return $attributes;
+    }
 
-		// Read the types and priorities from the database including
-		// results.
-		$types_include = $options['types_include'];
-		$types = array();
-		$types_results = array();
+    public function render_form($context)
+    {
+        $project = $context['project'];
 
-		if ($types_include && $run_ids)
-		{
-			$types = $this->_model->get_types();
-			foreach ($types as $type)
-			{
-				$types_results[$type->id] = 
-					$this->_model->get_type_results(
-						$run_ids,
-						$type->id
-					);
-			}
-		}
+        $params = array(
+            'controls' => $this->_controls,
+            'project' => $project,
+            'case_columns' => $context['case_columns'],
+            'case_groupby' => $this->_get_groupby(
+                $project->id,
+                $context['case_columns'],
+                $context['case_fields']
+            )
+        );
 
-		$priorities_include = $options['priorities_include'];
-		$priorities = array();
-		$priorities_results = array();
+        // Note that we return separate HTML snippets for the form/
+        // options and the used dialogs (which must be included after
+        // the actual form as they include their own <form> tags).
+        return array(
+            'form' => $this->render_view(
+                'form',
+                $params,
+                true
+            ),
+            'after_form' => $this->render_view(
+                'form_dialogs',
+                $params,
+                true
+            )
+        );
+    }
 
-		if ($priorities_include && $run_ids)
-		{
-			$priorities = $this->_model->get_priorities();
-			foreach ($priorities as $priority)
-			{
-				$priorities_results[$priority->id] = 
-					$this->_model->get_priority_results(
-						$run_ids,
-						$priority->id
-					);
-			}
-		}
+    public function run($context, $options)
+    {
+        $project = $context['project'];
 
-		// Render the report to a temporary file and return the path
-		// to TestRail (including additional resources that need to be
-		// copied).
-		return array(
-			'resources' => self::$_resources,
-			'html_file' => $this->render_page(
-				'index',
-				array(
-					'report' => $context['report'],
-					'project' => $project,
-					'runs' => $runs,
-					'run_rels' => $run_rels,
-					'run_count' => $run_count,
-					'statuses' => $statuses,
-					'types_include' => $types_include,
-					'types' => $types,
-					'types_results' => $types_results,
-					'priorities_include' => $priorities_include,
-					'priorities' => $priorities,
-					'priorities_results' => $priorities_results
-				)
-			)
-		);
-	}
-}
+        // We read the test suites first.
+        $suites = $this->_helper->get_suites_by_include(
+            $project->id,
+            $options['suites_ids'],
+            $options['suites_include']
+        );
 
-class Tests_cases_priorities_summary_model extends BaseModel
-{
-	public function get_statuses()
-	{
-		$this->db->select('*');
-		$this->db->from('statuses');
-		$this->db->where('is_active', true);
-		$this->db->order_by('display_order');
-		return $this->db->get_result();
-	}
+        $suite_ids = obj::get_ids($suites);
 
-	public function get_types()
-	{
-		$this->db->select('*');
-		$this->db->from('case_types');
-		$this->db->where('is_deleted', false);
-		$this->db->order_by('name', 'asc');
-		return $this->db->get_result();
-	}
+        // Limit this report to specific test cases, if requested.
+        // This is only relevant for single-suite projects and with a
+        // section filter.
+        $case_ids = $this->_helper->get_case_scope_by_include(
+            $suite_ids,
+            arr::get($options, 'sections_ids'),
+            arr::get($options, 'sections_include'),
+            $has_cases
+        );
 
-	public function get_type_results($run_ids, $type_id)
-	{
-		$query = $this->db->query(
-			'SELECT
-				tests.status_id,
-				COUNT(*) as status_count
-			FROM
-				tests
-			JOIN
-				cases
-					on
-				cases.id = tests.content_id
-			WHERE
-				tests.run_id in ({0}) and
-				cases.type_id = {1}
-			GROUP BY
-				tests.status_id',
-			$run_ids,
-			$type_id
-		);
+        // We then read the available groups in the scope and compute
+        // the case counts for these groups. If we also display the
+        // test cases we get a list of them for each group.
 
-		$results = $query->result();
-		return obj::get_lookup_scalar(
-			$results,
-			'status_id',
-			'status_count'
-		);
-	}
+        $show_summary = $options['cases_include_summary'];
+        $show_details = $options['cases_include_details'];
 
-	public function get_priorities()
-	{
-		$this->db->select('*');
-		$this->db->from('priorities');
-		$this->db->where('is_deleted', false);
-		$this->db->order_by('priority', 'asc');
-		return $this->db->get_result();
-	}
+        if ($suite_ids && $has_cases)
+        {
+            $case_groups = $this->_helper->get_case_groups_ex(
+                $suite_ids,
+                $case_ids,
+                $context['fields'],
+                $options['cases_groupby'],
+                $options['cases_filters']
+            );
 
-	public function get_priority_results($run_ids, $priority_id)
-	{
-		$query = $this->db->query(
-			'SELECT
-				tests.status_id,
-				COUNT(*) as status_count
-			FROM
-				tests
-			JOIN
-				cases
-					on
-				cases.id = tests.content_id
-			WHERE
-				tests.run_id in ({0}) and
-				cases.priority_id = {1}
-			GROUP BY
-				tests.status_id',
-			$run_ids,
-			$priority_id
-		);
+            $cases = array();
+            if ($show_details)
+            {
+                foreach ($case_groups as $group)
+                {
+                    $cases[$group->id] =
+                        $this->_helper->get_cases_by_group_ex(
+                            $suite_ids,
+                            $case_ids,
+                            $context['fields'],
+                            $options['cases_groupby'],
+                            $group->id,
+                            $options['cases_filters'],
+                            $options['cases_limit']
+                        );
+                }
+            }
+        }
+        else
+        {
+            $cases = array();
+            $case_groups = array();
+        }
 
-		$results = $query->result();
-		return obj::get_lookup_scalar(
-			$results,
-			'status_id',
-			'status_count'
-		);
-	}
+        // Render the report to a temporary file and return the path
+        // to TestRail (including additional resources that need to be
+        // copied).
+        return array(
+            'resources' => self::$_resources,
+            'html_file' => $this->render_page(
+                'index',
+                array(
+                    'report' => $context['report'],
+                    'project' => $project,
+                    'suites' => $suites,
+                    'cases' => $cases,
+                    'case_groups' => $case_groups,
+                    'case_groupby' => $options['cases_groupby'],
+                    'case_columns' => $context['case_columns'],
+                    'case_columns_for_user' =>
+                        $options['cases_columns'],
+                    'case_fields' => $context['case_fields'],
+                    'case_limit' => $options['cases_limit'],
+                    'show_summary' => $show_summary,
+                    'show_details' => $show_details,
+                    'show_links' => !$options['content_hide_links']
+                )
+            )
+        );
+    }
 }
