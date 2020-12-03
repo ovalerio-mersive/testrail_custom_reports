674728545b8d67c7b62632e66bb498e0d4c8b08c
ovalerio280788 <osvamo280788@gmail.com>
2020-12-03 16:47:19 -0600

    Working on the first custom report

--- Tests_cases_prioritiesv2/i18n/translations/en/reports_tmpl.php
+++ Tests_cases_prioritiesv2/i18n/translations/en/reports_tmpl.php
@@ -5,38 +5,48 @@ $lang['reports_tmpl_meta_group'] = 'Custom report';
 $lang['reports_tmpl_meta_summary'] = 'Custom report created to display a summary of the test cases mainly based on their priorities';
 $lang['reports_tmpl_meta_description'] = 'It displays a table with a summary (total, percentage) of test cases that belongs to each priority.';
 
+$lang['reports_cas_form_grouping'] = 'Grouping &amp; Changes';
+$lang['reports_cas_form_suites'] = 'Test Suites';
+$lang['reports_cas_form_sections'] = 'Sections';
+$lang['reports_cas_form_cases'] = 'Test Cases';
+$lang['reports_cas_form_cases_groupby'] = 'Group the changes by:';
+$lang['reports_cas_form_cases_groupby_day'] = 'Day';
+$lang['reports_cas_form_cases_groupby_month'] = 'Month';
+$lang['reports_cas_form_cases_groupby_cases'] = 'Test Cases';
+$lang['reports_cas_form_cases_groupby_suite'] = 'Test Suite';
+$lang['reports_cas_form_cases_groupby_invalid'] = 'Unknown or unsupported test case grouping option.';
 
-$lang['reports_cpg_form_grouping'] = 'Grouping';
+$lang['reports_cas_form_cases_changes'] = 'Include the following changes:';
+$lang['reports_cas_form_cases_include_new'] = 'New test cases';
+$lang['reports_cas_form_cases_include_updated'] = 'Updated test cases <em>(latest update only)</em>';
+$lang['reports_cas_form_cases_limit'] = 'Maximum number of test cases to display:';
+$lang['reports_cas_form_cases_required'] = 'Please include at least one change option (new or updated).';
 
-$lang['reports_cpg_form_cases'] = 'Test Cases';
-$lang['reports_cpg_form_cases_groupby'] = 'Group the test cases by:';
-$lang['reports_cpg_form_suites'] = 'Test Suites';
-$lang['reports_cpg_form_sections'] = 'Sections';
+$lang['reports_cas_suites_header'] = 'Test Suites';
+$lang['reports_cas_suites_header_info'] = 'Shows the list of test suites that are used for this report.';
+$lang['reports_cas_suites_empty'] = 'No test suites found.';
+$lang['reports_cas_changes_header'] = 'Changes';
+$lang['reports_cas_changes_header_info'] = 'Shows the new and/or updated test cases, grouped by the configured attribute.';
 
-$lang['reports_cpg_form_cases_details'] = 'Include the following details:';
-$lang['reports_cpg_form_cases_include_summary'] = 'An aggregated summary of groups';
-$lang['reports_cpg_form_cases_include_cases'] = 'The test case details per group';
-$lang['reports_cpg_form_cases_limit'] = 'Maximum number of test cases to display (per group):';
-$lang['reports_cpg_form_cases_required'] = 'Please check at least one test case option (summary or details).';
+$lang['reports_cas_cases_groupby_unknown'] = 'Unknown';
+$lang['reports_cas_cases_groupby_day'] = 'Day';
+$lang['reports_cas_cases_groupby_month'] = 'Month';
+$lang['reports_cas_cases_groupby_suite'] = 'Test Suite';
+$lang['reports_cas_cases_groupby_cases'] = 'Test Cases';
+$lang['reports_cas_cases_created'] = 'Created';
+$lang['reports_cas_cases_created_short'] = 'C';
+$lang['reports_cas_cases_updated'] = 'Updated';
+$lang['reports_cas_cases_updated_short'] = 'U';
+$lang['reports_cas_cases_percent'] = 'Percent';
+$lang['reports_cas_cases_empty'] = 'No changes found.';
+$lang['reports_cas_cases_more_created'] = 'There {0?{are}:{is}} {0} more new {0?{test cases}:{test case}} that {0?{are}:{is}} not displayed (in total).';
+$lang['reports_cas_cases_more_updated'] = 'There {0?{are}:{is}} {0} more updated {0?{test cases}:{test case}} that {0?{are}:{is}} not displayed (in total).';
+$lang['reports_cas_cases_change'] = 'Change';
+$lang['reports_cas_cases_changes'] = 'Changes';
 
-$lang['reports_cpg_suites_header'] = 'Test Suites';
-$lang['reports_cpg_suites_header_info'] = 'Shows the list of test suites that are used for this report.';
-$lang['reports_cpg_suites_empty'] = 'No test suites found.';
-
-$lang['reports_cpg_cases_header'] = 'Test Cases';
-$lang['reports_cpg_cases_cases'] = 'Test Cases';
-$lang['reports_cpg_cases_header_info'] = 'Shows the test cases for the configured project and test suites, grouped by the selected attribute.';
-$lang['reports_cpg_cases_name'] = 'Name';
-$lang['reports_cpg_cases_count'] = 'Count';
-$lang['reports_cpg_cases_percent'] = 'Percent';
-$lang['reports_cpg_cases_empty'] = 'No groups found.';
-$lang['reports_cpg_cases_more'] = 'There {0?{are}:{is}} {0} more {0?{test cases}:{test case}} in this group that {0?{are}:{is}} not displayed.';
-
-$lang['reports_cpg_cases_groupby_template'] = 'Template';
-$lang['reports_cpg_cases_groupby_priority'] = 'Priority';
-$lang['reports_cpg_cases_groupby_type'] = 'Type';
-$lang['reports_cpg_cases_groupby_milestone'] = 'Milestone';
-$lang['reports_cpg_cases_groupby_createdby'] = 'Created By';
-$lang['reports_cpg_cases_groupby_unknown'] = 'Unknown';
-
-$lang['reports_cpg_charts_bar_title'] = 'Test Cases by {0}';
+$lang['reports_cas_charts_bar_title'] = 'Changes by {0}';
+$lang['reports_cas_charts_bar_title_to'] = 'Changes by {0} (up to {1})';
+$lang['reports_cas_charts_bar_title_from'] = 'Changes by {0} (from {1})';
+$lang['reports_cas_charts_bar_title_from_to'] = 'Changes by {0} ({1} - {2})';
+$lang['reports_cas_charts_legend_created'] = 'Created';
+$lang['reports_cas_charts_legend_updated'] = 'Updated';
