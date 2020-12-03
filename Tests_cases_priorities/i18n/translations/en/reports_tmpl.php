2734c2d2c7944df8d401ee6ee02af7e8411acc6b
ovalerio280788 <osvamo280788@gmail.com>
2020-12-03 14:15:30 -0600

    Working on the first custom report

--- Tests_cases_priorities/i18n/translations/en/reports_tmpl.php
+++ Tests_cases_priorities/i18n/translations/en/reports_tmpl.php
@@ -6,26 +6,37 @@ $lang['reports_tmpl_meta_summary'] = 'Custom report created to display a summary
 $lang['reports_tmpl_meta_description'] = 'It displays a table with a summary (total, percentage) of test cases that belongs to each priority.';
 
 
-$lang['reports_tmpl_form_details'] = 'Details';
-$lang['reports_tmpl_form_details_include'] = 'Include statistics for the following entities:';
-$lang['reports_tmpl_form_details_include_types'] = 'Types';
-$lang['reports_tmpl_form_details_include_priorities'] = 'Priorities';
-$lang['reports_tmpl_form_details_include_required'] = 'At least one option must be selected (Types or Priorities)';
-$lang['reports_tmpl_form_runs'] = 'Test Suites &amp; Runs';
+$lang['reports_cpg_form_grouping'] = 'Grouping';
 
-$lang['reports_tmpl_runs_header'] = 'Test Runs';
-$lang['reports_tmpl_runs_header_info'] = 'Shows the test runs used for this report.';
-$lang['reports_tmpl_runs_empty'] = 'No test runs found.';
-$lang['reports_tmpl_runs_more'] = 'There {0?{are}:{is}} {0} more {0?{test runs}:{test run}} for the test suite(s) that {0?{are}:{is}} not included.';
+$lang['reports_cpg_form_cases'] = 'Test Cases';
+$lang['reports_cpg_form_cases_groupby'] = 'Group the test cases by:';
+$lang['reports_cpg_form_suites'] = 'Test Suites';
+$lang['reports_cpg_form_sections'] = 'Sections';
 
-$lang['reports_tmpl_types_header'] = 'Types';
-$lang['reports_tmpl_types_header_info'] = 'Shows a matrix for the test case types with their status counts for the configured scope (test runs and project).';
-$lang['reports_tmpl_types_empty'] = 'No test case types found.';
-$lang['reports_tmpl_types_item'] = 'Type';
+$lang['reports_cpg_form_cases_details'] = 'Include the following details:';
+$lang['reports_cpg_form_cases_include_summary'] = 'An aggregated summary of groups';
+$lang['reports_cpg_form_cases_include_cases'] = 'The test case details per group';
+$lang['reports_cpg_form_cases_limit'] = 'Maximum number of test cases to display (per group):';
+$lang['reports_cpg_form_cases_required'] = 'Please check at least one test case option (summary or details).';
 
-$lang['reports_tmpl_priorities_header'] = 'Priorities';
-$lang['reports_tmpl_priorities_header_info'] = 'Shows a matrix for the priorities with their status counts for the configured scope (test runs and project).';
-$lang['reports_tmpl_priorities_empty'] = 'No priorities found.';
-$lang['reports_tmpl_priorities_item'] = 'Priority';
+$lang['reports_cpg_suites_header'] = 'Test Suites';
+$lang['reports_cpg_suites_header_info'] = 'Shows the list of test suites that are used for this report.';
+$lang['reports_cpg_suites_empty'] = 'No test suites found.';
 
-$lang['reports_tmpl_table_total'] = 'Total';
+$lang['reports_cpg_cases_header'] = 'Test Cases';
+$lang['reports_cpg_cases_cases'] = 'Test Cases';
+$lang['reports_cpg_cases_header_info'] = 'Shows the test cases for the configured project and test suites, grouped by the selected attribute.';
+$lang['reports_cpg_cases_name'] = 'Name';
+$lang['reports_cpg_cases_count'] = 'Count';
+$lang['reports_cpg_cases_percent'] = 'Percent';
+$lang['reports_cpg_cases_empty'] = 'No groups found.';
+$lang['reports_cpg_cases_more'] = 'There {0?{are}:{is}} {0} more {0?{test cases}:{test case}} in this group that {0?{are}:{is}} not displayed.';
+
+$lang['reports_cpg_cases_groupby_template'] = 'Template';
+$lang['reports_cpg_cases_groupby_priority'] = 'Priority';
+$lang['reports_cpg_cases_groupby_type'] = 'Type';
+$lang['reports_cpg_cases_groupby_milestone'] = 'Milestone';
+$lang['reports_cpg_cases_groupby_createdby'] = 'Created By';
+$lang['reports_cpg_cases_groupby_unknown'] = 'Unknown';
+
+$lang['reports_cpg_charts_bar_title'] = 'Test Cases by {0}';
