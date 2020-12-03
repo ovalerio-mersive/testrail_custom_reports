6077590f2b71c6332fc396f6ae559faf0291dbd9
ovalerio280788 <osvamo280788@gmail.com>
2020-12-02 23:14:29 -0600

    Working on the first custom report

--- Tests_oscar_report_v2/views/index.php
+++ Tests_oscar_report_v2/views/index.php
@@ -99,7 +99,7 @@ $report_obj->render_view('index/charts/status', $temp);
 		$temp['items'] = $types;
 		$temp['results'] = $types_results;
 		$temp['statuses'] = $statuses;
-		$report_obj->render_view('index/table', $temp);
+		$report_obj->render_view('index/table', $temp, $statuses);
 		?>
 	<? else: ?>
 		<p><?= lang('reports_tpr_types_empty') ?></p>
