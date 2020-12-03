7ba1933731d567a2bc1c62988257cc2ce4e17c0b
ovalerio280788 <osvamo280788@gmail.com>
2020-12-02 21:36:40 -0600

    Working on the first custom report

---
+++ tr_v6.6/Tests_oscar_report_v2/i18n/translations/en/reports_tmpl.php
@@ -0,0 +1,17 @@
+<?php
+
+$lang['reports_tmpl_meta_label'] = 'Oscar Report #2';
+$lang['reports_tmpl_meta_group'] = 'Custom Reports';
+$lang['reports_tmpl_meta_summary'] = 'Demonstrates how to develop custom report plugins.';
+$lang['reports_tmpl_meta_description'] = 'Demonstrates how to develop custom report plugins and serves as a skeleton for new report plugins.';
+
+$lang['reports_tmpl_form_intro'] = 'This section can be fully customized by the report plugin and is used to configure the report specific options (e.g. scope, time frame, etc.).';
+$lang['reports_tmpl_form_details'] = 'Details';
+$lang['reports_tmpl_form_runs'] = 'Test Suites & Runs';
+
+
+$lang['reports_tmpl_form_details_include'] = 'Include the following entities';
+$lang['reports_tmpl_form_details_include_types'] = 'Types';
+$lang['reports_tmpl_form_details_include_priorities'] = 'Priorities';
+
+$lang['reports_tmpl_form_details_include_required'] = 'You need to select one of these options: Types or Priorities';
