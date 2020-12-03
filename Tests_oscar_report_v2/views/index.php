06c97c01e1e5bd1fe4ed64b2605a81523e839f98
ovalerio280788 <osvamo280788@gmail.com>
2020-12-02 23:23:25 -0600

    Working on the first custom report

--- Tests_oscar_report_v2/views/index.php
+++ Tests_oscar_report_v2/views/index.php
@@ -1,4 +1,29 @@
-<?php
-$min_width = 250;
+$min_width = 960;
+ 
+$header = array(
+	'project' => $project,
+	'report' => $report,
+	'meta' => $report_obj->get_meta(),
+	'min_width' => $min_width,
+	'css' => array(
+		'styles/reset.css' => 'all',
+		'styles/view.css' => 'all',
+		'styles/print.css' => 'print'
+	),
+	'js' => array(
+		'js/jquery.js'
+	)
+);
+ 
+$GI->load->view('report_plugins/layout/header', $header);
 ?>
-Hola
+ 
+The report content goes here.
+<h1>Hola</h1>
+ 
+$temp = array();
+$temp['report'] = $report;
+$temp['meta'] = $report_obj->get_meta();
+$temp['show_options'] = true;
+$temp['show_report'] = true;
+$GI->load->view('report_plugins/layout/footer', $temp);
