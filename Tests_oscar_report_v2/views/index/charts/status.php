80cc77d698586322cef50449215b752fb53075a1
ovalerio280788 <osvamo280788@gmail.com>
2020-12-03 00:04:53 -0600

    Working on the first custom report

--- Tests_oscar_report_v2/views/index/charts/status.php
+++ Tests_oscar_report_v2/views/index/charts/status.php
@@ -1,11 +1,3 @@
 <?php
-print_r(statuses);
-?>
-<? $statuses = $GI->cache->get_objects('status') ?>
-
-<?php
-$temp = array();
-$temp['stats'] = $stats;
-$temp['statuses'] = $statuses;
-$GI->load->view('report_plugins/charts/status', $temp);
+print_r($statuses);
 ?>
