d9ed651a03bee1541deb79972aae20cb70451731
ovalerio280788 <osvamo280788@gmail.com>
2020-12-04 16:46:10 -0600

    Working on the first custom report

--- Tests_cases_prioritiesv2/views/index/table.php
+++ Tests_cases_prioritiesv2/views/index/table.php
@@ -10,7 +10,6 @@
 		<th><?=h( $header )?></th>
 		<?php foreach ($cases as $case): ?>
 			<th style="text-align: right">
-				<!-- <span class="statusBox" style="<?= tests::get_status_box_colors($status->color_dark) ?>">&nbsp;&nbsp;</span> -->
 				<span class="statusBox">&nbsp;&nbsp;</span>
 				<?=h( $case->id ) ?>
 			</th>
