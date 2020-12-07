9a20296bb4721fd88cb128304499a790940a879d
ovalerio280788 <osvamo280788@gmail.com>
2020-12-07 16:37:28 -0600

    Working on the first custom report

--- Tests_cases_prioritiesv2/views/index/table.php
+++ Tests_cases_prioritiesv2/views/index/table.php
@@ -28,7 +28,7 @@
 		<?php
 			echo "<td>" . $section_id . "</td>";
 			foreach($items as $item) {
-				echo "<td>" . var_dump($item) . "</td>"; 
+				echo "<td>" . print_r($item) . "</td>"; 
 			}
 		 	// echo "<td>" . key($sections) . " -- Count obj: " . $sections[key($sections)]->priority_count . "</td>";
 		?>
