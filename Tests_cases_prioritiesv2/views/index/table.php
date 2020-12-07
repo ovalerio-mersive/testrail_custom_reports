bd5e59ef8b7483a158898a46437b584b4aeddeb5
ovalerio280788 <osvamo280788@gmail.com>
2020-12-07 16:28:56 -0600

    Working on the first custom report

--- Tests_cases_prioritiesv2/views/index/table.php
+++ Tests_cases_prioritiesv2/views/index/table.php
@@ -26,7 +26,7 @@
 		<?php $alt = arr::alternator('odd', 'even') ?>
 		<tr class="<?= $alt ?>">
 		<?php
-		echo "<td>" . section_id . "</td>"
+		echo "<td>" . $section_id . "</td>"
 		 	// echo "<td>" . key($sections) . " -- Count obj: " . $sections[key($sections)]->priority_count . "</td>";
 		?>
 		</tr>
