9f7984da3bb4bee91009ef91f71aea76eecfec9a
ovalerio280788 <osvamo280788@gmail.com>
2020-12-07 17:46:17 -0600

    Working on the first custom report

--- Tests_cases_prioritiesv2/views/index/table.php
+++ Tests_cases_prioritiesv2/views/index/table.php
@@ -1,8 +1,8 @@
 <?php print_r($total_automated_cases); ?>
-<?php print_r($total_automated_cases); ?>
+<?php print_($total_automated_cases); ?>
 <table class="grid">
 <caption style="caption-side:bottom;text-align:right;">
-	<h3><?= lang('reports_tmpl_table_automated_p1') . ": " . $total_automated_cases; ?> </h3>
+	<h3><?= lang('reports_tmpl_table_automated_p1') ?> <?php echo $total_automated_cases; ?> </h3>
 	<h3><?= lang('reports_tmpl_table_automated_total') ?>: </h3>
 </caption>
 	<tr class="header">
