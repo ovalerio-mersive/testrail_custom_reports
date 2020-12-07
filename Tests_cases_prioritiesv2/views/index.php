4ca578d493fe5a2a4370888a5b15510c559ed70b
ovalerio280788 <osvamo280788@gmail.com>
2020-12-07 13:38:10 -0600

    Working on the first custom report

--- Tests_cases_prioritiesv2/views/index.php
+++ Tests_cases_prioritiesv2/views/index.php
@@ -44,9 +44,10 @@ foreach($suite_ids as $s) {
 				$stats->suite_name = $c->section_name;
 				$stats->priority_name = $c->p_name;
 			}
-			if ($c->c_p_id == $p->p_id) {
-				$stats->priority_count += 1;
-			}
+			print_r($c);
+			// if ($c->c_p_id == $p->p_id) {
+			// 	$stats->priority_count += 1;
+			// }
 		}
 	}
 	array_push($summary, $stats);
