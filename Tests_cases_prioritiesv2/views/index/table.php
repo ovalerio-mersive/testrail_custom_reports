70b1b437ba24da591fd6bc087232cb869886cbe5
ovalerio280788 <osvamo280788@gmail.com>
2020-12-07 22:37:53 -0600

    Working on the first custom report

--- Tests_cases_prioritiesv2/views/index/table.php
+++ Tests_cases_prioritiesv2/views/index/table.php
@@ -2,7 +2,7 @@
 <caption style="caption-side:bottom;text-align:right;">
 <div class="card">
 	<div class="row">
-		<?php echo "<h3>" . lang('reports_tmpl_table_automated_p1') . ": <h4 class='mr-3 display-4 float-right'> ". $automated_p1_cases[0]->total_automated_tcs_with_priority ." / 20% </h4></h3>"; ?>		
+		<?php echo "<h3>" . lang('reports_tmpl_table_automated_p1') . "</h3>: <label class='mr-3 display-4 float-right'> ". $automated_p1_cases[0]->total_automated_tcs_with_priority ." / 20% </label>"; ?>		
 		<?php echo "<h3>" . lang('reports_tmpl_table_automated_total') . ": <h4 class='ml-3 display-4 float-left'> ". $total_automated_cases[0]->total_automated_tcs ." / 20% </h4></h3>"; ?>
 	</div>
 </div>
