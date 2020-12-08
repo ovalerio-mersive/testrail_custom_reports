e41d4bf5988ba0bba729d5760f0e81008d6b3c21
ovalerio280788 <osvamo280788@gmail.com>
2020-12-07 22:35:29 -0600

    Working on the first custom report

--- Tests_cases_prioritiesv2/views/index/table.php
+++ Tests_cases_prioritiesv2/views/index/table.php
@@ -2,12 +2,8 @@
 <caption style="caption-side:bottom;text-align:right;">
 <div class="card">
 	<div class="row">
-		<div class="col-6">
-			<?php echo "<h3>" . lang('reports_tmpl_table_automated_p1') . ": <h4 class='mr-3 display-4 float-right'> ". $automated_p1_cases[0]->total_automated_tcs_with_priority ." / 20% </h4></h3>"; ?>
-		</div>
-		<div class="col-6">
-			<?php echo "<h3>" . lang('reports_tmpl_table_automated_total') . ": <h4 class='ml-3 display-4 float-left'> ". $total_automated_cases[0]->total_automated_tcs ." / 20% </h4></h3>"; ?>
-		</div>
+		<?php echo "<h3>" . lang('reports_tmpl_table_automated_p1') . ": <h4 class='mr-3 display-4 float-right'> ". $automated_p1_cases[0]->total_automated_tcs_with_priority ." / 20% </h4></h3>"; ?>		
+		<?php echo "<h3>" . lang('reports_tmpl_table_automated_total') . ": <h4 class='ml-3 display-4 float-left'> ". $total_automated_cases[0]->total_automated_tcs ." / 20% </h4></h3>"; ?>
 	</div>
 </div>
 </caption>
