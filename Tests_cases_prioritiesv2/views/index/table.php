902e0f9805f5490f6f86e9ea4a7c34ba917d6089
ovalerio280788 <osvamo280788@gmail.com>
2020-12-07 22:00:22 -0600

    Working on the first custom report

--- Tests_cases_prioritiesv2/views/index/table.php
+++ Tests_cases_prioritiesv2/views/index/table.php
@@ -1,5 +1,15 @@
 <table class="grid">
 <caption style="caption-side:bottom;text-align:right;">
+<div class="card">
+  <div class="card-header">
+    Featured
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
 <?php
 	echo "<h3>" . lang('reports_tmpl_table_automated_p1') . ": " . $automated_p1_cases[0]->total_automated_tcs_with_priority . " ( x% )</h3>";
 	echo "<h3>" . lang('reports_tmpl_table_automated_total') . ": " . $total_automated_cases[0]->total_automated_tcs . " ( x% )</h3>";
