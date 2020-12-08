82b7d77990e57850434895a3ae6f8327e366d31f
ovalerio280788 <osvamo280788@gmail.com>
2020-12-08 02:38:49 -0600

    Working on the first custom report

--- Tests_cases_prioritiesv2/views/index/table.php
+++ Tests_cases_prioritiesv2/views/index/table.php
@@ -2,7 +2,7 @@
 <caption style="caption-side:bottom;text-align:right;">
 <div class="card p-2">
 	<label style="font-size:13;"><?php echo lang('reports_tmpl_table_total_cases') . ": <b>". $total_cases . "</b>"; ?></label><br/>
-	<label style="font-size:13;"><?php echo lang('reports_tmpl_table_automated_p1') . ": <b>". $automated_p1_cases[0]->total_automated_tcs_with_priority ."/" . round(($automated_p1_cases[0]->total_automated_tcs_with_priority*100)/$total_cases) . "%</b>"; ?></label><br/>
+	<label style="font-size:13;"><?php echo lang('reports_tmpl_table_automated_p1') . ": <b>". $automated_p1_cases[0]->total_automated_tcs_with_priority ." case(s) (" . round(($automated_p1_cases[0]->total_automated_tcs_with_priority*100)/$total_cases) . "%)</b>"; ?></label><br/>
 	<label style="font-size:13;"><?php echo lang('reports_tmpl_table_automated_total') . ": <b>". $total_automated_cases[0]->total_automated_tcs ."/20%</b>"; ?></label>
 </div>
 </caption>
