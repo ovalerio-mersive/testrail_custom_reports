fa2f0dc542e4bdf7f5060815de372215de1b5aa9
ovalerio280788 <osvamo280788@gmail.com>
2020-12-08 02:00:35 -0600

    Working on the first custom report

--- Tests_cases_prioritiesv2/report.php
+++ Tests_cases_prioritiesv2/report.php
@@ -167,7 +167,8 @@ class Tests_cases_prioritiesv2_summary_model extends BaseModel
 
 			echo "<br/><br/><br/>";
 			print_r($tmp_result);
-			print_r($result->depth);
+			echo " ----- ";
+			print_r($tmp_result->depth);
 			// while ($result->depth > 0) {
 			// 	// get the parent information
 			// 	$query = $this->db->query(
