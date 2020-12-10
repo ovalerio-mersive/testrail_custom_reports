3c6afba4e7315a47a6b73b8b6821218b4cefc69b
ovalerio280788 <osvamo280788@gmail.com>
2020-12-10 14:18:04 -0600

    Working on the first custom report

--- Tests_cases_prioritiesv2/views/index/table.php
+++ Tests_cases_prioritiesv2/views/index/table.php
@@ -4,8 +4,12 @@
 	<table class="table">
 		<tbody>
 			<tr>
-				<th>HOla</th>
-				<td>HOla</td>
+				<th> <?php echo lang('reports_tmpl_table_total_cases'); ?> </th>
+				<th> <?php echo lang('reports_tmpl_table_automated_p1') ?> </td>
+			</tr>
+			<tr>
+				<td><?php echo $total_cases; ?></td>
+				<td><?php echo $automated_p1_cases[0]->total_automated_tcs_with_priority ." case(s) (" . round(($automated_p1_cases[0]->total_automated_tcs_with_priority*100)/$total_cases); ?></td>
 			</tr>
 		</tbody>
 	</table>
