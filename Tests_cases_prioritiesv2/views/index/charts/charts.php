e6b63e3470cf929c0bffbbb1ab3ca1ed373a358d
ovalerio280788 <osvamo280788@gmail.com>
2020-12-09 23:12:06 -0600

    Working on the first custom report

--- Tests_cases_prioritiesv2/views/index/charts/charts.php
+++ Tests_cases_prioritiesv2/views/index/charts/charts.php
@@ -39,7 +39,22 @@ $(function () {
                     text: 'Source: <a href="http://en.wikipedia.org/wiki/List_of_cities_proper_by_population">Wikipedia</a>'
                 },
                 xAxis: {
-                    categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+                    // categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+                    categories: [
+                        <?php $is_first = true ?>
+                        <?php foreach ($priorities as $p): ?>
+                            <?php if (!$is_first): ?>
+                            ,
+                            <?php endif ?>
+                            <?php $category = h($p->name) ?>
+                            <?php echo  js::encode_string($category)?>
+                            <?php $is_first = false ?>
+                        <?php endforeach ?>
+                    ],
+                    tickmarkPlacement: 'on',
+                    title: {
+                        enabled: false
+                    }
                 },
                 yAxis: {
                     min: 0,
